74f15d0c6313ce9ffe3515865a17f1e7ee5c9366
Daniel Díaz González <danidiaz@dhcp053.crya.wireless>
2015-06-22 11:49:55 -0500

    Dani

--- test/fftdani/loaddata.py
+++ test/fftdani/loaddata.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib
+#matplotlib.use('TKAgg')
+
 
 fftsize=1024
 freq = 2048e6
@@ -32,13 +35,34 @@ def update_data (num, data, aux):
 	aux = plt.plot([num],[y_value],'ro')
 	return aux
 
-previous= 0
-aux = generate_data()
-fig1 = plt.figure()
-plt.xlim(0, fftsize)
-plt.ylim(-200, 20)
-print (int(frames*1./freq))
-#line_ani = animation.FuncAnimation(fig1, update_data, rates, fargs=(data, aux), interval=int(1000.0/frames), blit=True, repeat=False)
-line_ani = animation.FuncAnimation(fig1, update_data, rates, fargs=(data, None), interval=1, blit=True, repeat=False)
-plt.show()
+def bytime():
+	dataFile = np.fromfile('./fftusrp.dat', np.float32)
+	if debug:
+		print "Data:", dataFile
+	rates = dataFile.size /fftsize /frames
+	rates = rates - 1
+	data = np.empty([rates, fftsize*frames])
+	averages = np.empty(rates)
+	for i in range(rates):
+		aux = dataFile[i*(fftsize*frames):(i+1)*fftsize*frames]
+		data[i] = (np.asarray(aux))
+		averages[i] = np.average(data[i])
+	if debug:
+		print "Averages:", averages
+	
+	fig1 = plt.figure()
+	plt.plot(averages)
+	plt.show()
+
+bytime()
+
+	
+
+#aux = generate_data()
+#fig1 = plt.figure()
+#plt.xlim(0, fftsize)
+#plt.ylim(-200, 20)
+##line_ani = animation.FuncAnimation(fig1, update_data, rates, fargs=(data, aux), interval=int(1000.0/frames), blit=True, repeat=False)
+#line_ani = animation.FuncAnimation(fig1, update_data, rates, fargs=(data, None), interval=1, blit=True, repeat=False)
+#plt.show()
 
