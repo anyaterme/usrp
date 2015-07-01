0d0845fac1528354dd5a1a7d19f80ad94cb4f5fb
Daniel Díaz González <danidiaz@dhcp065.crya.wireless>
2015-07-01 16:52:40 -0500

    Dani

--- test/fftdani/danifft.py
+++ test/fftdani/danifft.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 ##################################################
-# Gnuradio Python Flow Graph
+# GNU Radio Python Flow Graph
 # Title: Danifft
-# Generated: Tue Jun  9 14:54:10 2015
+# Generated: Wed Jul  1 16:45:54 2015
 ##################################################
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
 from PyQt4 import Qt
-from PyQt4.QtCore import QObject, pyqtSlot
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
@@ -16,12 +25,13 @@ from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.fft import logpwrfft
 from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
-import PyQt4.Qwt5 as Qwt
 import sip
 import sys
 import time
 
+from distutils.version import StrictVersion
 class danifft(gr.top_block, Qt.QWidget):
 
     def __init__(self):
@@ -51,17 +61,17 @@ class danifft(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.tunning = tunning = 432.1e6
-        self.samp_rate = samp_rate = 8e6
         self.range_freq = range_freq = 0
+        self.tunning = tunning = 50e6+range_freq*1e6
+        self.samp_rate = samp_rate = 8e6
         self.fft_size = fft_size = 1024
 
         ##################################################
         # Blocks
         ##################################################
         self.uhd_usrp_source_0 = uhd.usrp_source(
-        	device_addr="",
-        	stream_args=uhd.stream_args(
+        	",".join(("", "")),
+        	uhd.stream_args(
         		cpu_format="fc32",
         		channels=range(1),
         	),
@@ -71,29 +81,9 @@ class danifft(gr.top_block, Qt.QWidget):
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_center_freq(tunning, 0)
         self.uhd_usrp_source_0.set_gain(10, 0)
-        self._range_freq_layout = Qt.QVBoxLayout()
-        self._range_freq_tool_bar = Qt.QToolBar(self)
-        self._range_freq_layout.addWidget(self._range_freq_tool_bar)
-        self._range_freq_tool_bar.addWidget(Qt.QLabel("range_freq"+": "))
-        class qwt_counter_pyslot(Qwt.QwtCounter):
-            def __init__(self, parent=None):
-                Qwt.QwtCounter.__init__(self, parent)
-            @pyqtSlot('double')
-            def setValue(self, value):
-                super(Qwt.QwtCounter, self).setValue(value)
-        self._range_freq_counter = qwt_counter_pyslot()
-        self._range_freq_counter.setRange(-50, 50, 1)
-        self._range_freq_counter.setNumButtons(2)
-        self._range_freq_counter.setValue(self.range_freq)
-        self._range_freq_tool_bar.addWidget(self._range_freq_counter)
-        self._range_freq_counter.valueChanged.connect(self.set_range_freq)
-        self._range_freq_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._range_freq_slider.setRange(-50, 50, 1)
-        self._range_freq_slider.setValue(self.range_freq)
-        self._range_freq_slider.setMinimumWidth(200)
-        self._range_freq_slider.valueChanged.connect(self.set_range_freq)
-        self._range_freq_layout.addWidget(self._range_freq_slider)
-        self.top_layout.addLayout(self._range_freq_layout)
+        self._range_freq_range = Range(-50, 50, 1, 0, 200)
+        self._range_freq_win = RangeWidget(self._range_freq_range, self.set_range_freq, "range_freq", "counter_slider", float)
+        self.top_layout.addWidget(self._range_freq_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
         	fft_size, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -104,6 +94,35 @@ class danifft(gr.top_block, Qt.QWidget):
         )
         self.qtgui_freq_sink_x_0.set_update_time(1)
         self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_freq_sink_x_0.disable_legend()
+        
+        if float == type(float()):
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+        
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self.logpwrfft_x_0 = logpwrfft.logpwrfft_f(
@@ -117,7 +136,7 @@ class danifft(gr.top_block, Qt.QWidget):
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
         self.blocks_null_sink_0_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*fft_size, "/home/laboratorio/develop/usrp/test/fftdani/fftusrp.dat", False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*fft_size, "./fftusrp.dat", False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
@@ -127,50 +146,47 @@ class danifft(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.logpwrfft_x_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_null_sink_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_complex_to_float_0, 0))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_null_sink_0_0, 0))
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.logpwrfft_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_null_sink_0_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 0), (self.logpwrfft_x_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_null_sink_0, 0))    
+        self.connect((self.logpwrfft_x_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_complex_to_float_0, 0))    
 
-
-# QT sink close method reimplementation
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "danifft")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
-
-    def get_tunning(self):
-        return self.tunning
-
-    def set_tunning(self, tunning):
-        self.tunning = tunning
-        self.uhd_usrp_source_0.set_center_freq(self.tunning, 0)
-        self.analog_sig_source_x_0.set_frequency(self.tunning)
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.logpwrfft_x_0.set_sample_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
 
     def get_range_freq(self):
         return self.range_freq
 
     def set_range_freq(self, range_freq):
         self.range_freq = range_freq
-        Qt.QMetaObject.invokeMethod(self._range_freq_counter, "setValue", Qt.Q_ARG("double", self.range_freq))
-        Qt.QMetaObject.invokeMethod(self._range_freq_slider, "setValue", Qt.Q_ARG("double", self.range_freq))
+        self.set_tunning(50e6+self.range_freq*1e6)
+
+    def get_tunning(self):
+        return self.tunning
+
+    def set_tunning(self, tunning):
+        self.tunning = tunning
+        self.analog_sig_source_x_0.set_frequency(self.tunning)
+        self.uhd_usrp_source_0.set_center_freq(self.tunning, 0)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.logpwrfft_x_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_fft_size(self):
         return self.fft_size
@@ -178,18 +194,12 @@ class danifft(gr.top_block, Qt.QWidget):
     def set_fft_size(self, fft_size):
         self.fft_size = fft_size
 
+
 if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
-    Qt.QApplication.setGraphicsSystem(gr.prefs().get_string('qtgui','style','raster'))
+    if(StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0")):
+        Qt.QApplication.setGraphicsSystem(gr.prefs().get_string('qtgui','style','raster'))
     qapp = Qt.QApplication(sys.argv)
     tb = danifft()
     tb.start()
@@ -200,4 +210,3 @@ if __name__ == '__main__':
     qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
     qapp.exec_()
     tb = None #to clean up Qt widgets
-
